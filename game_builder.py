737330a75a6735a52d684cdafcdb07e7eea9381b
hphilamore <hemmaphilamore@gmail.com>
2017-11-16 18:49:12 +0900

    draft 1 complete for meeting with Molly

--- game_builder.py
+++ game_builder.py
@@ -2,8 +2,8 @@ import random
 import pygame, sys
 from pygame.locals import *
 
-#l_score = 0
-#r_score = 0
+x = 0
+y = 1
 
 
 def ball_init(right):
@@ -20,10 +20,10 @@ def ball_init(right):
 
 
 def init():
-    "Vertically centres the paddles"
+    "Sets score to zero and sets random ball direction"
     global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel,l_score,r_score
-    paddle1_pos = [PAD_WIDTH//2 - 1,HEIGHT//2]
-    paddle2_pos = [WIDTH +1 - PAD_WIDTH//2,HEIGHT//2]
+    #paddle1_pos = [PAD_WIDTH//2 - 1,HEIGHT//2]
+    #paddle2_pos = [WIDTH +1 - PAD_WIDTH//2,HEIGHT//2]
     l_score = 0
     r_score = 0
     if random.randrange(0,2) == 0:
@@ -35,21 +35,24 @@ def init():
 def draw_board(colour=(255,255,255)):
     "Draws the marker lines in the colour specified"  
     global window, PAD_WIDTH, HEIGHT, WIDTH
-    pygame.draw.line(window,   colour, [WIDTH // 2, 0],[WIDTH / 2, HEIGHT], 1)
-    pygame.draw.line(window,   colour, [PAD_WIDTH, 0],[PAD_WIDTH, HEIGHT], 1)
-    pygame.draw.line(window,   colour, [WIDTH - PAD_WIDTH, 0],[WIDTH - PAD_WIDTH, HEIGHT], 1)
-    pygame.draw.circle(window, colour, [WIDTH//2, HEIGHT//2], HEIGHT//6, 1)
+    pygame.draw.line(window,   colour, [WIDTH // 2, 0],       [WIDTH // 2, HEIGHT], 1)
+    pygame.draw.line(window,   colour, [PAD_WIDTH, 0],        [PAD_WIDTH, HEIGHT], 1)
+    pygame.draw.line(window,   colour, [WIDTH - PAD_WIDTH, 0],[WIDTH - PAD_WIDTH,HEIGHT], 1)
+    pygame.draw.circle(window, colour, [WIDTH//2, HEIGHT//2],  HEIGHT//6, 1)
 
 
 def draw_ball(radius, colour=(0,255,0), *,
-              bounce_vert=True, bounce_horiz=False):
+              random_movement = False, 
+              bounce_vert=False, 
+              bounce_horiz=False):
 
     "Draws a circle of the radius specified, centered about the point specified"
 
     global window, l_score, r_score, BALL_POS, ball_vel
 
-    BALL_POS[x] += int(ball_vel[x])
-    BALL_POS[y] += int(ball_vel[y])
+    if random_movement:
+        BALL_POS[x] += int(ball_vel[x])
+        BALL_POS[y] += int(ball_vel[y])
 
     pygame.draw.circle(window, colour, BALL_POS, radius)
 
@@ -62,13 +65,13 @@ def draw_ball(radius, colour=(0,255,0), *,
         if int(BALL_POS[x]) <= radius         : ball_vel[x] = -ball_vel[x]
         if int(BALL_POS[x]) >= WIDTH - radius  : ball_vel[x] = -ball_vel[x]
 
-    if int(BALL_POS[x]) <= BALL_RADIUS: 
-        r_score += 1
-        ball_init(True)        
+    # if int(BALL_POS[x]) <= BALL_RADIUS: 
+    #     #r_score += 1
+    #     ball_init(True)        
 
-    elif int(BALL_POS[x]) > WIDTH - BALL_RADIUS:
-        l_score += 1
-        ball_init(False)
+    # elif int(BALL_POS[x]) > WIDTH - BALL_RADIUS:
+    #     #l_score += 1
+    #     ball_init(False)
 
 
 def draw_paddle(pad_height, pad_width, paddle_pos, paddle_vel, colour=(0,255,0)):
@@ -77,9 +80,13 @@ def draw_paddle(pad_height, pad_width, paddle_pos, paddle_vel, colour=(0,255,0))
     global window
 
     # if paddle in not off the board, or touching the edge but moving towards the centre, keep moving
-    if ((paddle_pos[y] > pad_height//2 and paddle_pos[y] < HEIGHT - pad_height//2) or
-        (paddle_pos[y] == pad_height//2 and paddle_vel[y] > 0) or
-        (paddle_pos[y] == HEIGHT - PAD_HEIGHT//2 and paddle_vel[y] < 0)):
+    # if ((paddle_pos[y] >= pad_height/2 and paddle_pos[y] <= HEIGHT - pad_height/2) or
+    #     (paddle_pos[y] == pad_height/2 and paddle_vel[y] > 0) or
+    #     (paddle_pos[y] == HEIGHT - PAD_HEIGHT/2 and paddle_vel[y] < 0)):
+
+    if ((paddle_pos[y] >= pad_height/2 and paddle_pos[y] <= HEIGHT - pad_height/2) or
+        (paddle_pos[y] < pad_height/2 and paddle_vel[y] > 0) or
+        (paddle_pos[y] > HEIGHT - PAD_HEIGHT/2 and paddle_vel[y] < 0)):
 
         paddle_pos[y] += paddle_vel[y]
 
@@ -105,6 +112,7 @@ def bounce_pad_collision(paddle_pos):
 
 
 def ball_reset_on_win():
+    "Recentres the ball when a point is scored by calling the ball initialisation function"
 
     global l_score, r_score, BALL_POS, BALL_RADIUS        
 
@@ -115,7 +123,11 @@ def ball_reset_on_win():
         ball_init(False)
 
 
-def update_scores(font="Comic Sans MS", font_size=20):
+def update_scores(*,font="Comic Sans MS", font_size=20):
+    "Increments and dislpays scores when a opint is scored"
+    # Available fonts: "Arial",  "Comic Sans MS", "monospace"
+    # pygame.font.get_fonts() returns all fonts
+
     global l_score, r_score, BALL_POS, BALL_RADIUS        
 
     if int(BALL_POS[x]) <= BALL_RADIUS: 
--- pong_functional.py
+++ pong_functional.py
@@ -25,11 +25,9 @@ ball_vel = [0,0]
 paddle1_vel = [0,0]
 paddle2_vel = [0,0]
 BALL_POS = [WIDTH//2,HEIGHT//2]
-paddle1_pos = [PAD_WIDTH//2 - 1,        HEIGHT//2]
-paddle2_pos = [WIDTH +1 - PAD_WIDTH//2, HEIGHT//2]
+paddle1_pos = [WIDTH - PAD_WIDTH//2, HEIGHT//2]
+paddle2_pos = [PAD_WIDTH//2,         HEIGHT//2]
 
-game_builder.x = x
-game_builder.y = y
 game_builder.WIDTH = WIDTH
 game_builder.HEIGHT = HEIGHT 
 game_builder.PAD_WIDTH = PAD_WIDTH
@@ -40,47 +38,41 @@ game_builder.paddle2_pos = paddle2_pos
 
 
 #canvas declaration
-window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+window = pygame.display.set_mode((WIDTH, HEIGHT))
 game_builder.window = window
-
-def draw():
-    global paddle1_pos, paddle2_pos
-           
-    window.fill(BLACK)
-    draw_board()
-    draw_ball(BALL_RADIUS, colour=(0,255,0))
-    draw_paddle(PAD_HEIGHT, PAD_WIDTH, paddle1_pos, paddle1_vel, colour=(0,0,255))
-    draw_paddle(PAD_HEIGHT, PAD_WIDTH, paddle2_pos, paddle2_vel, colour=(0,0,255))
-    print(paddle1_pos, paddle2_pos) 
-    bounce_pad_collision(paddle1_pos)
-    bounce_pad_collision(paddle2_pos)
-    update_scores()  
-    ball_reset_on_win() 
-    
 
 init()
 
 #game loop
 while True:
 
-    draw()
-
     event = pygame.event.poll()
 
     if event.type == pygame.QUIT:
         pygame.quit()
         sys.exit()
+           
+    window.fill(BLACK)
+    draw_board()
+    
+    draw_paddle(PAD_HEIGHT, PAD_WIDTH, paddle1_pos, paddle1_vel, colour=(0,0,255))
+    draw_paddle(PAD_HEIGHT, PAD_WIDTH, paddle2_pos, paddle2_vel, colour=(0,0,255))
+    draw_ball(BALL_RADIUS, colour=(0,255,0), random_movement = True, bounce_vert=True)
+    bounce_pad_collision(paddle1_pos)
+    bounce_pad_collision(paddle2_pos)
+    update_scores()  
+    ball_reset_on_win()     
 
     pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_UP] & pressed[pygame.K_DOWN]: paddle2_vel[y] = 0
-    elif pressed[pygame.K_UP]: paddle2_vel[y] = -8
-    elif pressed[pygame.K_DOWN]: paddle2_vel[y] = 8
-    else: paddle2_vel[y] = 0
-
-    if pressed[pygame.K_w] & pressed[pygame.K_s]: paddle1_vel[y] = 0
-    elif pressed[pygame.K_w]: paddle1_vel[y] = -8
-    elif pressed[pygame.K_s]: paddle1_vel[y] = 8
+    if pressed[pygame.K_UP] & pressed[pygame.K_DOWN]: paddle1_vel[y] = 0
+    elif pressed[pygame.K_UP]: paddle1_vel[y] = -8
+    elif pressed[pygame.K_DOWN]: paddle1_vel[y] = 8
     else: paddle1_vel[y] = 0
+
+    if pressed[pygame.K_w] & pressed[pygame.K_s]: paddle2_vel[y] = 0
+    elif pressed[pygame.K_w]: paddle2_vel[y] = -8
+    elif pressed[pygame.K_s]: paddle2_vel[y] = 8
+    else: paddle2_vel[y] = 0
             
     pygame.display.update()
     clock.tick(60)
